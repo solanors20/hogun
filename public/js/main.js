8a4395fc2de99d696bec66a8f37aa24463161157
Steven Solano <solanors20@gmail.com>
2016-09-09 15:10:26 -0600

    Autenticacion y ui actualizada

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,4 @@
-var adminSite = angular.module('TicketSystem', ['ngRoute']);
+var adminSite = angular.module('TicketSystem', ['ngRoute', 'angular-table']);
 
 // configure our routes
     adminSite.config(function($routeProvider) {
@@ -8,6 +8,14 @@ var adminSite = angular.module('TicketSystem', ['ngRoute']);
             .when('/', {
                 templateUrl : 'resumen.html',
                 controller  : 'resumenController'
+            })
+            .when('/resumen', {
+                templateUrl : 'resumen.html',
+                controller  : 'resumenController'
+            })
+            .when('/tiquetes', {
+                templateUrl : 'tiquetes.html',
+                controller  : 'tiquetesController'
             })
     });
 
@@ -39,6 +47,46 @@ var prettyDate =myDate.getFullYear() + '/' +(myDate.getMonth()+1) + '/' + myDate
             });
     };
 
+    });
+
+    adminSite.controller('tiquetesController', function($scope, $http) { 
+        $scope.config = {
+            itemsPerPage: 15,
+            fillLastPage: true
+        } 
+        $http.get('/buses/listar')
+            .success(function(data) {
+
+                $scope.buses = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+
+            $http.get('/rutas/dropdown')
+            .success(function(data) {
+
+                $scope.rutas = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+
+            $scope.obtenerTiquetes = function() {
+                $scope.formData.busSeleccionado = $scope.busSeleccionado;
+                $scope.formData.rutaSeleccionada = $scope.rutaSeleccionada;
+        $http.post('/tiquetes/listar', $scope.formData)
+            .success(function(data) {
+
+                $scope.tiquetes = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
     });
 
 
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');    // pull information from HTML POST (express4)
+var multer = require('multer');
 var mysql = require('mysql');
+var fs = require("fs");
+var jwt    = require('jsonwebtoken');
+var config = require('./config'); // get our config file
 var connection = mysql.createConnection({
    host: 'localhost',
    user: 'root',
@@ -9,10 +13,108 @@ var connection = mysql.createConnection({
    database: 'busticketdb',
    port: 3306
 });
-    // Localización de los ficheros estÃ¡ticos
-    app.use(express.static(__dirname + '/public'));
-	app.use(bodyParser.json());                                     // parse application/json
-app.use(bodyParser.urlencoded({extended: true}));  
+// Configuring Passport
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var expressSession = require('express-session');
+var bCrypt = require('bcrypt-nodejs');
+var flash = require('connect-flash');
+
+//app.set('superSecret', config.secret); // secret variable
+/**app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "http://localhost");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});*/
+
+// Localización de los ficheros estaticos
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.json({limit: '50mb'}));                                     // parse application/json
+app.use(bodyParser.urlencoded({extended: true, limit: '50mb'}));
+
+app.use(expressSession({secret: 'aff123s',
+    resave: true,
+    saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(flash());
+
+
+// Passport needs to be able to serialize and deserialize users to support persistent login sessions
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+	connection.query('SELECT * FROM usuario where id = ?', [id], function(error, filas, resultado){
+    if(error){
+       return done(error);
+    }else{
+       if(resultado.length > 0){
+       	done(error, filas[0]);
+       }
+    }
+  });
+});
+
+var isValidPassword = function(user, password){
+        return bCrypt.compareSync(password, user.clave);
+}
+
+var isAuthenticated = function (req, res, next) {
+	// if user is authenticated in the session, call the next() to call the next request handler 
+	// Passport adds this method to request object. A middleware is allowed to add properties to
+	// request and response objects
+	if (req.isAuthenticated())
+		return next();
+	// if the user is not authenticated then redirect him to the login page
+	res.redirect('/');
+}
+
+app.get('/signout', function(req, res) {
+  req.logout();
+  res.redirect('/');
+});
+// passport/login.js
+passport.use('login', new LocalStrategy({
+    passReqToCallback : true
+  },
+  function(req, username, password, done) { 
+    connection.query('SELECT * FROM usuario where cedula = ?', [username], function(error, filas, resultado){
+      if(error){
+         return done(error);
+      }else{
+         if(filas.length > 0){
+         	if (!isValidPassword(filas[0], password)){
+	          return done(null, false, 
+	              req.flash('message', 'Invalid Password'));
+	        }
+	        // User and password both match, return user from 
+	        // done method which will be treated like success
+	        return done(null, filas[0]);
+         }else{
+          	return done(null, false, 
+                req.flash('message', 'User Not found.')); 
+         }
+      }
+   });
+}));
+
+
+
+app.post('/login', passport.authenticate('login', {
+		successRedirect: '/home',
+		failureRedirect: '/',
+		failureFlash : true  
+}));
+
+app.get('/', function(req, res) { 
+    res.sendFile(__dirname + '/public/auth.html');            
+});
+
+app.get('/home', isAuthenticated, function(req, res){
+	res.sendFile(__dirname + '/public/home.html');
+});
 
 app.get('/tarifas/listar/', function (req, res) {
 	if(req.query.placa){
@@ -60,6 +162,21 @@ app.get('/paradas/listar/', function (req, res) {
 	}
 });
 
+
+app.get('/rutas/listar/', function (req, res) {
+	if(req.query.placa){
+	 	connection.query('CALL listarRutasPorBus(?)', [req.query.placa], function(error, filas){
+	      	if(error){
+	         	res.send(error);
+	      	}else{
+	      		res.json(filas[0]);
+	      	}
+   		});	
+	}else{
+		res.json([]);
+	}
+});
+
 app.post('/tiquetes/agregar', function	(req, res){
 	var info = req.body.data;
 	if(req.body.data){
@@ -75,50 +192,99 @@ app.post('/tiquetes/agregar', function	(req, res){
 	}
 });
 
+app.post('/cedulas/subir', function(request, response) {
+  	var bitmap = new Buffer(request.body.imagen, 'base64');
+  	var fecha = new Date();
+  	var mes = fecha.getMonth() + 1;
+  	var anio = fecha.getFullYear();
+  	var directorio = "./public/cedulas/" + mes + "_" + anio +"/";
+  	var respuesta = {};
+  	var error = false;
+  	try{
+  		if(fsExisteSync(directorio)){
+  		if(!fsExisteSync(directorio + request.body.nombre)){
+  			fs.writeFileSync(directorio + request.body.nombre, bitmap);
+  		}
+	  	}else{
+	  		fs.mkdirSync(directorio);
+	  		fs.writeFileSync(directorio + request.body.nombre, bitmap);
+	  	}
+  	}catch(e){
+  		error = true;
+  	}
+  	if(!error){
+  		respuesta = {"Success" : "Se guardo la imagen con exito."};	
+  	}
+  	response.json(respuesta);
+});
+
+function fsExisteSync(ruta) {
+  try {
+    fs.accessSync(ruta);
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
 
 
 
 
-
-app.get('/resumen', function (req, res) {
+app.get('/resumen', isAuthenticated, function (req, res) {
 	connection.query('SELECT Ruta.nombre, SUM(TOTAL) total FROM Tiquete inner join Ruta on Tiquete.ruta = Ruta.idRuta', function(error, filas, resultado){
       if(error){
-         res.send(error);
+        res.send(error);
       }else{
-         if(resultado.length > 0){
-            res.json(filas);
-         }else{
-            console.log('Registro no encontrado');
-         }
+        res.json(filas);
       }
    });
 
-})
+});
 
 
-app.post('/resumen', function (req, res) {
-	console.log(req.body.fecha);
+app.post('/resumen', isAuthenticated, function (req, res) {
+	
 connection.query('SELECT Ruta.nombre, SUM(TOTAL) total FROM Tiquete inner join Ruta on Tiquete.ruta = Ruta.idRuta where date(fecha) = ?', [req.body.fecha], function(error, filas, resultado){
       if(error){
          res.send(error);
       }else{
-         if(resultado.length > 0){
-            res.json(filas);
-         }else{
-            console.log('Registro no encontrado');
-         }
+         res.json(filas);
       }
    });
-})
-
-
-
-
-app.get('/', function(req, res) {  
-    res.sendFile('./public/index.html');            
 });
 
-var server = app.listen(8081, function () {
+app.get('/buses/listar', isAuthenticated, function(req, res){
+  connection.query('SELECT id, placa FROM bus b inner join busesxusuario bu on b.id = bu.bus where usuario = ?', [req.user.id], function(error, filas, resultado){
+      if(error){
+        res.send(error);
+      }else{
+        res.json(filas);
+      }
+   });
+});
+
+app.get('/rutas/dropdown', isAuthenticated, function (req, res) {
+  
+connection.query('SELECT r.idRuta, r.nombre FROM ruta r inner join busesxruta br on r.idRuta = br.ruta inner join bus b on br.bus = b.id inner join busesxusuario bu on b.id = bu.bus where usuario = ?', [req.user.id], function(error, filas, resultado){
+      if(error){
+        res.send(error);
+      }else{
+        res.json(filas);
+      }
+   });
+});
+
+app.post('/tiquetes/listar', isAuthenticated, function (req, res) {
+connection.query('SELECT DATE_FORMAT(fecha, \'\%d-\%m-\%Y \%h:\%i \%p\') as fecha, a.nombre as origen, b.nombre as destino, t.total, t.numeroPasajes as cantidad, t.adultoMayor as cedula FROM Tiquete t inner join Region a on t.origen = a.idRegion inner join Region b on t.origen = b.idRegion where date(fecha) = ? and t.bus = ? and t.ruta = ?', [req.body.fecha, req.body.busSeleccionado.id, req.body.rutaSeleccionada.idRuta], function(error, filas, resultado){
+      if(error){
+         res.send(error);
+      }else{
+        res.json(filas);
+      }
+   });
+});
+
+var server = app.listen(8080, function () {
 
   var host = server.address().address
   var port = server.address().port
